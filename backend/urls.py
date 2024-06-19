755067c0fe7751fd6ce0e3eecc95bb8c0b5a843b
Christian Donat <c.donat.de@gmail.com>
2024-06-19 22:23:35 +0200

    KAN-12 ADD analysis app

--- backend/urls.py
+++ backend/urls.py
@@ -3,12 +3,9 @@ from django.conf.urls import include
 from django.contrib import admin
 from django.urls import path
 
-# Remove this when you start your proejct
-from django.views import debug
-
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', debug.default_urlconf),
+    path("", include("analyser.urls")),
+    path('admin/', admin.site.urls)
 ]
 
 if settings.DEBUG:
